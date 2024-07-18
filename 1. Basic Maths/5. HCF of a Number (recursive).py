aab0984f101ce28e2d2c95be3c15fd6d36b14986
Kushal Kapoor <60167153+k-kushal07@users.noreply.github.com>
2024-07-18 18:23:39 +0530

    feat: Implemented HCF of Numbers
    - added problem description, sample input and outputs

---
+++ 1. Basic Maths/5. HCF of a Number (recursive).py
@@ -0,0 +1,19 @@
+"""
+Question:
+Write a Python function to find the highest common factor (HCF) or greatest common divisor (GCD) of two given non-negative integers. The function should take two integers
+a and b as input and return their HCF or GCD.
+"""
+"""
+Input:
+12 15
+Output:
+3
+"""
+
+def hcf(num1, num2):
+    if num2:
+        return hcf(num2, num1 % num2)
+    return num1
+
+num1, num2, *_ = [int(item) for item in input("Enter the numbers: ").split()]
+print(f"HCF of numbers {num1}, {num2} is: ", hcf(num1, num2))
